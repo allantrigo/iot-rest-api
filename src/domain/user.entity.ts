c280e2dc11b68484009f8d7e68423c43b59551bb
Allan Trigo <allantrigo@gec.inatel.br>
2023-06-17 16:18:40 -0300

    feat: start nest

---
+++ src/domain/user.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, OneToMany } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { DeviceToUser } from './device-to-user.entity';
+
+@Entity({ name: 'user' })
+export class User extends BaseEntity {
+  @Column({ type: 'varchar', length: 30 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  password: string;
+
+  @OneToMany(() => DeviceToUser, (deviceToUser) => deviceToUser.user)
+  public deviceToUser: DeviceToUser[];
+}
